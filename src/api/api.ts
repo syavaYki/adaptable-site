622d28a700ab0c9146c4d8a3831dcad8559afb77
syavaYki <s_podolskiy@live.com>
2025-07-03 19:28:32 -0400

    updated BE host for restore to http

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_PET_API_BASE_URL,
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
