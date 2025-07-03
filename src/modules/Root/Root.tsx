91d2ca5285ca274040fdb0a0a3289a67d0d1707a
syavaYki <s_podolskiy@live.com>
2025-07-03 19:13:19 -0400

    updated BE host

--- src/modules/Root/Root.tsx
+++ src/modules/Root/Root.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ApolloProvider } from '@apollo/client';
 import {
   // BrowserRouter as Router,
   HashRouter as Router,
@@ -10,7 +9,6 @@ import {
 import { HomePage } from '../HomePage';
 import { CatalogPage } from '../CatalogPage';
 import App from '../App/App';
-import client from '../../lib/apollos';
 import { ProtectedRoute } from '../../components/ProtectedRoute/ProtectedRoute';
 import { VALID_ROUTES } from '../../types/validRoutes';
 import { FavoritePage } from '../FavoritePage';
@@ -24,89 +22,87 @@ import { UnsubscribePage } from '../UnsubscribePage';
 
 export const Root = () => {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <Routes>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<App />}
+        >
           <Route
-            path="/"
-            element={<App />}
-          >
-            <Route
-              index
-              element={<HomePage />}
-            />
+            index
+            element={<HomePage />}
+          />
 
-            <Route
-              path={VALID_ROUTES.HOME}
-              element={
-                <Navigate
-                  to="/"
-                  replace
-                />
-              }
-            />
+          <Route
+            path={VALID_ROUTES.HOME}
+            element={
+              <Navigate
+                to="/"
+                replace
+              />
+            }
+          />
 
-            <Route
-              path={VALID_ROUTES.CATALOG}
-              element={<CatalogPage />}
-            />
+          <Route
+            path={VALID_ROUTES.CATALOG}
+            element={<CatalogPage />}
+          />
 
-            <Route
-              path={VALID_ROUTES.FAVORITES}
-              element={<FavoritePage />}
-            />
+          <Route
+            path={VALID_ROUTES.FAVORITES}
+            element={<FavoritePage />}
+          />
 
-            <Route
-              path={VALID_ROUTES.LOGIN}
-              element={<LogInPage />}
-            />
+          <Route
+            path={VALID_ROUTES.LOGIN}
+            element={<LogInPage />}
+          />
 
-            <Route
-              path={VALID_ROUTES.REGISTER}
-              element={<RegisterPage />}
-            />
+          <Route
+            path={VALID_ROUTES.REGISTER}
+            element={<RegisterPage />}
+          />
 
-            <Route
-              path={VALID_ROUTES.PASSWORD_RESET}
-              element={<ResetPasswordPage />}
-            />
+          <Route
+            path={VALID_ROUTES.PASSWORD_RESET}
+            element={<ResetPasswordPage />}
+          />
 
-            <Route
-              path={VALID_ROUTES.UNSUBSCRIBE}
-              element={<UnsubscribePage />}
-            />
+          <Route
+            path={VALID_ROUTES.UNSUBSCRIBE}
+            element={<UnsubscribePage />}
+          />
 
-            <Route
-              path={VALID_ROUTES.HOW_TO_HELP}
-              element={<HowToHelpPage />}
-            />
+          <Route
+            path={VALID_ROUTES.HOW_TO_HELP}
+            element={<HowToHelpPage />}
+          />
 
-            <Route
-              path="/pets/:id"
-              element={<PetInfoPage />}
-            />
+          <Route
+            path="/pets/:id"
+            element={<PetInfoPage />}
+          />
 
-            <Route
-              path="/pets/"
-              element={<CatalogPage />}
-            />
+          <Route
+            path="/pets/"
+            element={<CatalogPage />}
+          />
 
-            <Route element={<ProtectedRoute />}>
-              <Route path={VALID_ROUTES.ACCOUNT}>
-                <Route
-                  index
-                  element={<AccountPage />}
-                />
-              </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path={VALID_ROUTES.ACCOUNT}>
+              <Route
+                index
+                element={<AccountPage />}
+              />
             </Route>
           </Route>
+        </Route>
 
-          <Route
-            path="*"
-            element={<p>PAGE NOT FOUND</p>}
-          />
-        </Routes>
-      </Router>
-    </ApolloProvider>
+        <Route
+          path="*"
+          element={<p>PAGE NOT FOUND</p>}
+        />
+      </Routes>
+    </Router>
   );
 };
